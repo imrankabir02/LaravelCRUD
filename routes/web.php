1a5ecb45ea6edd9040ee8f39ec786344db1d3716
imrankabir02 <imrankabir325@gmail.com>
2023-05-19 18:01:01 +0600

    Product Create

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('product')->group(function () {
+    Route::get('/',[ProductController::class, 'create'])->name('create');
+    Route::post('/create',[ProductController::class, 'store'])->name('product-create');
 });
